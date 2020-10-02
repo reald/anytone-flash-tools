a31518a66ceda3b656b1edf02168fffeaba74f2c
Dennis Real <github@tildepipe.org>
2020-10-02 19:15:54 +0200

    Anytone 878uv emulator for flashing protocol analysis

---
+++ emulator/at_878uv_emulator.py
@@ -0,0 +1,148 @@
+#!python3
+#
+# Emulate anytone 878uv radio to customer programming software. 
+# Send intercepted data stream over network to server script for further investigation.
+#
+# This script connects to a virtual com port COM26 which is connected via a virtual
+# null modem cable to the virtual com port COM18 which is used by the programming software.
+# This virtual ports and cable can be provided by the COM0COM tool.
+
+
+import serial
+import time
+import socket
+import sys
+
+# config
+servername = '192.168.1.2' # ip or hostname of server
+serverport = 2342
+portname = 'COM26' # connected to COM18 with com0com. use COM18 in CPS
+
+
+
+
+
+# open serial port
+serialPort = None
+
+try:
+   serialPort = serial.Serial(port = portname, baudrate=4000000, bytesize=8, timeout=1, stopbits=serial.STOPBITS_ONE) # 115200 921600 4000000
+except:
+   print('ERR: Could not open port ' + portname)
+   exit()
+
+
+# make tcp connection
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (servername, serverport)
+sock.connect(server_address)
+
+
+
+# wait for data
+
+try:
+   
+   while 1:
+
+      command = ''
+
+      command = serialPort.read()
+      while serialPort.in_waiting > 0:
+         command += serialPort.read()
+
+      # print("Command " + str(command) )
+   
+      # mirror command to server as hex string
+      if ( len(command) > 0 ):
+         sock.sendall( (command.hex() + '\n').encode() )
+
+      # respond to command on com port
+      if ( len(command) == 0 ):
+         pass
+
+      elif ( command == b'PROGRAM'):
+         print("Program session requested.")
+         resp = b'QX\x06'
+         serialPort.write(resp)
+         sock.sendall( (resp.hex() + '\n').encode() )
+
+      elif ( command == b'\x02' ):
+         print("Device info requested.")
+         resp = b'ID878UV\x00\x00V100\x00\x00\x06'
+         serialPort.write(resp)
+         sock.sendall( (resp.hex() + '\n').encode() )
+
+      elif ( command == b'R\x02\xfa\x00\x20\x10' ):
+         print("Read special memory request.")
+         resp = b'W\x02\xfa\x00\x20\x10\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00\x24\x06'
+         serialPort.write(resp)
+         sock.sendall( (resp.hex() + '\n').encode() )
+         
+      elif ( command[0:4] == b'R\x02\xfa\x00' and command[5] == 16 ):
+         # 0x02fa00..
+         print("Read local information.")
+         
+         resp = b'W\x02\xfa\x00' + bytes([command[4]]) + b'\x10'
+         
+         if ( command[4] == 0x00 ):
+            resp += b'\x00\x00\x00\x00\x01\x01\x01\x00\x00\x01\x01\x20\x20\x20\x20\xff'
+                                           
+         elif ( command[4] == 0x10 ): # Radio Type
+            resp += b'\x44\x38\x37\x38\x55\x56\x00\x01\x00\xff\xff\xff\xff\xff\xff\xff'
+
+         elif ( command[4] == 0x30 ): # Serial Number
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0x40 ): # Production Date
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0x50 ): # Manucfacture Code
+            resp += b'\x31\x32\x33\x34\x35\x36\x37\x38\xff\xff\xff\xff\xff\xff\xff\xff'
+         elif ( command[4] == 0x60 ): # Maintained Date
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0x70 ): # Dealer Code
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0x80 ): # Stock Date
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0x90 ): # Sell Date
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0xa0 ): # Seller
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+         elif ( command[4] == 0xb0 ): # Maintained Description
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0xc0 ): 
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0xd0 ): 
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0xe0 ): 
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+         elif ( command[4] == 0xf0 ): 
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+         else:
+            resp += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+         resp = resp + bytes( [sum(resp[1:]) & 0xff] ) + b'\x06'
+         #print(resp.hex())
+         serialPort.write(resp)
+         sock.sendall( (resp.hex() + '\n').encode() )
+
+      elif ( command[0] == ord('W') ) :
+         print("write request.")
+         resp = b'\x06' # just ack
+         serialPort.write(resp)
+         sock.sendall( (resp.hex() + '\n').encode() )
+
+      elif ( command == b'END' ):
+         print("End session.")
+         resp = b'\x06' # just ack
+         serialPort.write(resp)
+         sock.sendall( (resp.hex() + '\n').encode() )
+
+
+
+finally:
+   print('QRT')
+   serialPort.close()
+   sock.close()
+
---
+++ emulator/at_878uv_server.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+#
+# receive intercepted programming data from at878uv emulator via network
+# and diff hexdumps
+
+
+import socket
+import sys
+from datetime import datetime
+import pipes
+import os
+
+
+# config
+myservername = '0.0.0.0' # 0.0.0.0 means listen on all interfaces
+mylistenport = 2342 # port name to listen on
+pathcreatehexdump = '/home/user/anytone-flash-tools/createhexdump.py'
+datadir = '/tmp'
+comparetool = 'meld'
+
+
+# vars
+numconnects = 0
+datasetname = ''
+lastfilename = ''
+
+
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = (myservername, mylistenport)
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+
+while True:
+   # Wait for a connection
+   print('Waiting for a connection...', file=sys.stderr)
+   connection, client_address = sock.accept()
+
+    
+   try:
+       print ('Connection from', client_address, file=sys.stderr)
+
+       line = ''
+       alldata = ''
+
+       # Receive data
+       while True:
+
+          data = connection.recv(1)
+
+          #print ('received "%s"' % data, file=sys.stderr)
+
+          if data:
+             
+             if data != b'\n':
+                line += data.decode('latin1')
+                # print(line)
+                
+             else:
+                # EOL received
+
+                alldata += line + '\n'
+
+                
+                if ( line == '50524f4752414d' ): # "PROGRAM"
+                   print("Programming session started!", file=sys.stderr)
+                   numconnects += 1
+                   datasetname = datetime.now().strftime("%y%m%d-%H%M%S-") + str(numconnects)
+
+                elif ( line == '454e44' ): # "END"
+                   print("Programming session ended!", file=sys.stderr)
+                   #print(datasetname)
+                   
+                   p = pipes.Template()
+                   p.append(pathcreatehexdump + ' - > $OUT', '-f')
+                   # p.debug(True)
+                   
+                   outfilename = datadir + '/' + datasetname + '.data'
+                   f = p.open(outfilename, 'w')
+                   try:
+                      f.write(alldata)
+                   finally:
+                      f.close()
+
+                   #print(alldata)
+                   
+                   if ( len(lastfilename) > 0 ):
+                      # compare last 2 dumps
+                      print('Starting compare ' + lastfilename + ' ' + outfilename)
+                      os.spawnlp(os.P_NOWAIT, comparetool, comparetool, lastfilename , outfilename)
+                   
+                   alldata = ''
+                   lastfilename = outfilename
+                   datasetname = ''
+                   
+                else:
+                   pass
+
+                   
+                line = ''
+             
+          else:
+             print ('No more data from', client_address, file=sys.stderr)
+             break
+            
+   finally:
+      # Clean up the connection
+      connection.close()
+
